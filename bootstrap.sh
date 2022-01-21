a773e65732c55781b69350ba1f4fbd61a1ce3e9d
srujanm111 <srujanmup@gmail.com>
2022-01-21 14:35:44 -0600

    bootstrap with symlink prompt

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,54 @@
 #!/usr/bin/env zsh
 
+echo "Bootstrapping..."
+
 # get directory script is located in
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
+# function to prompt user if a file needs to be overwritten by a symlink
+symlink () {
+  SRC_FILE="$1"
+  SYMLINK_FILE="$2"
+
+  # if the symlink file location already contains a file or symlink
+  if [[ -f "$SYMLINK_FILE" ]] || [[ -h "$SYMLINK_FILE" ]]; then
+    # if the file is a symlink that already points to the right source file, return
+    if [[ -L "$SYMLINK_FILE" ]] && [[ "$(readlink "$SYMLINK_FILE")" == "$SRC_FILE" ]]; then
+      return;
+    fi
+
+    # prompt the user
+    while true
+    do
+      read "ANS?$SYMLINK_FILE already exists. Overwrite it? (y/n): "
+      case "$ANS" in
+        [yY]*) rm -f "$SYMLINK_FILE"; break;;
+        [nY]*) return;;
+        *) echo "Please enter y/n"
+      esac
+    done
+
+  fi
+
+  # make the symlink
+  ln -s "$SRC_FILE" "$SYMLINK_FILE"
+}
+
+# SYMLINKS
+
 # zsh
-ln -s $DIR/zsh/zlogin "${ZDOTDIR:-$HOME}"/.zlogin
-ln -s $DIR/zsh/zlogout "${ZDOTDIR:-$HOME}"/.zlogout
-ln -s $DIR/zsh/zpreztorc "${ZDOTDIR:-$HOME}"/.zpreztorc
-ln -s $DIR/zsh/zprofile "${ZDOTDIR:-$HOME}"/.zprofile
-ln -s $DIR/zsh/zshenv "${ZDOTDIR:-$HOME}"/.zshenv
-ln -s $DIR/zsh/zshrc "${ZDOTDIR:-$HOME}"/.zshrc
-ln -s $DIR/zsh/p10k.zsh "${ZDOTDIR:-$HOME}"/.p10k.zsh
+symlink "$DIR"/zsh/zlogin "${ZDOTDIR:-$HOME}"/.zlogin
+symlink "$DIR"/zsh/zlogout "${ZDOTDIR:-$HOME}"/.zlogout
+symlink "$DIR"/zsh/zpreztorc "${ZDOTDIR:-$HOME}"/.zpreztorc
+symlink "$DIR"/zsh/zprofile "${ZDOTDIR:-$HOME}"/.zprofile
+symlink "$DIR"/zsh/zshenv "${ZDOTDIR:-$HOME}"/.zshenv
+symlink "$DIR"/zsh/zshrc "${ZDOTDIR:-$HOME}"/.zshrc
+symlink "$DIR"/zsh/p10k.zsh "${ZDOTDIR:-$HOME}"/.p10k.zsh
 
 # vim
-ln -s $DIR/vim/vimrc $HOME/.vimrc
-ln -s $DIR/vim/mytheme.vim $HOME/.vim/colors/mytheme.vim
+symlink "$DIR"/vim/vimrc "$HOME"/.vimrc
+symlink "$DIR"/vim/mytheme.vim "$HOME"/.vim/colors/mytheme.vim
 
 # git
-ln -s $DIR/git/gitconfig $HOME/.gitconfig
-ln -s $DIR/git/gitignore_global $HOME/.gitignore_global
+symlink "$DIR"/git/gitconfig "$HOME"/.gitconfig
+symlink "$DIR"/git/gitignore_global "$HOME"/.gitignore_global
